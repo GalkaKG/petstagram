71c728d7ad59f880765d63c223e6112f1c6ada1f
GalkaKG <galina_georgieva_net@abv.bg>
2023-02-14 12:20:56 +0200

    Modified

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -9,13 +9,13 @@ def login(request):
     return None
 
 
-def show_profile_details(request):
-    return None
+def show_profile_details(request, pk):
+    return render(request, template_name='accounts/profile-details-page.html')
 
 
 def edit_profile(request):
-    return None
+    return render(request, template_name='accounts/profile-edit-page.html')
 
 
 def delete_profile(request):
-    return None
+    return render(request, template_name='accounts/profile-delete-page.html')
