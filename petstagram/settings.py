28d919de057ea0025aff008a5dc9e362f5fc8663
GalkaKG <galina_georgieva_net@abv.bg>
2023-06-16 15:50:36 +0300

    new commit

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram_database',
+        'USER': 'postgres-user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
