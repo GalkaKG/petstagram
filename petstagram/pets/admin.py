beb06286c7fb99c47be36a8a7503b0df511c2afe
GalkaKG <galina_georgieva_net@abv.bg>
2023-06-16 18:19:55 +0300

    new commit

--- petstagram/pets/admin.py
+++ petstagram/pets/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from petstagram.pets.models import Pet
 
-# Register your models here.
+
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+admin.site.register(Pet, PetAdmin)
--- petstagram/photos/models.py
+++ petstagram/photos/models.py
@@ -1,3 +1,32 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from petstagram.pets.models import Pet
+
+
+class Photo(models.Model):
+    photo = models.ImageField()
+
+    description = models.TextField(
+        max_length=300,
+        validators=(
+            MinLengthValidator(10),
+        ),
+        blank=True,
+        null=True,
+    )
+
+    location = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    tagged_pets = models.ManyToManyField(
+        Pet,
+        blank=True,
+    )
+
+    date_of_publication = models.DateField(
+        auto_now=True,
+    )
